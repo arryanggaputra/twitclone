53907ebf2ea18609bf11710a903be5f4ced5bb4f
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2020-03-05 10:48:53 +0700

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+// postcss.config.js
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.vue",
+    "./public/**/*.jsx"
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          purgecss,
+          require("cssnano")({
+            preset: "default"
+          })
+        ]
+      : [])
+  ]
+};
