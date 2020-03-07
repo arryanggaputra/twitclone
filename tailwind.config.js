5ef8726173180640038f7e8cec3220b53ef826fd
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2020-03-07 21:42:48 +0700

    Fix login page with background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ const multipleLinesPlugin = require("tailwindcss-plugin-multiple-lines");
 
 module.exports = {
   theme: {
+    zIndex: {
+      "-1": "-1"
+    },
     extend: {}
   },
   variants: {},
